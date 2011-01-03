1f068daed9f6b7b67427faeb9d872f132130d244
Michael P. Geraci <michael@reducify.com>
2011-01-03 13:20:54 -0600

    moved javascript files to folder, added json parse and utility file for patterns

--- js/patterns.js
+++ js/patterns.js
@@ -1,11 +1,34 @@
 $(function(){
+  max();
   normalize();
-})
+});
+
+a = patterns[6].data;
+
+// normalize an array of coordinates to 0,0
+function max(){
+  // add all the x's to a new array
+  x = [];
+
+  for (i=0; i < a.length; i++) {
+    x.push(a[i][0]);
+  }
+
+  // add all the y's to a new array
+  y = [];
+
+  for (i=0; i < a.length; i++) {
+    y.push(a[i][1]);
+  }
+  
+  maxX = Math.max.apply(Math, x) + 1;
+  maxY = Math.max.apply(Math, y) + 1;
+
+  $('body').append('width: ' + maxX + '<br>height: ' + maxY + '<br>');
+}
 
 // normalize an array of coordinates to 0,0
 function normalize(){
-  a = [[7,24],[8,22],[8,24],[9,12],[9,13],[9,20],[9,21],[9,28],[9,29],[10,11],[10,15],[10,20],[10,21],[10,28],[10,29],[11,0],[11,1],[11,10],[11,16],[11,20],[11,21],[12,0],[12,1],[12,10],[12,14],[12,16],[12,17],[12,22],[12,24],[13,10],[13,16],[13,24],[14,17],[14,21],[15,18],[15,19]]
-
   // add all the x's to a new array
   x = [];
 
